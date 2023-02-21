e1508dfa16fecae8f2e75eca1db0e833ca8d955d
hisham <hishammpsn@gmail.com>
2023-02-21 22:27:37 +0530

    header background image fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,14 +18,14 @@ function Home() {
     return (
         <>
 
-            <div className="h-full bg-no-repeat bg-contain " style={{ backgroundImage: `url(${background})` }} >
+            <div className="h-full bg-no-repeat bg-contain " style={{ backgroundImage: `url(${background}) `,backgroundSize:"100vw 40vh" }} >
                 <Header SetsideNav={SetsideNav} Navdata={Navdata} />
                 <div onClick={()=> SetsideNav(false)}>
                 {sidenav ? <SideNav Navdata={Navdata} /> : <></>}
                     <div className=' md:grid md:grid-cols-5 mb-30 p-10 flex flex-col-reverse'>
                         <div className='  col-span-5 md:col-span-3 w-full h-full md:flex justify-center text-center m-auto flex-col'>
                             <p className='cursor-pointer hidden md:block  m-20 font-bold font-sans text-[40px] tracking-[2px] text-white hover:text-gray-500 '>Careers</p>
-                            <div className='text-start md:ml-28'>
+                            <div className='text-start md:ml-28 '>
                                 <p className='font-bold font-sans sm:text-[40px] text-[30px] tracking-[2px] text-[#20404a] '>Submit Your Resume</p>
                                 <p className='font-sans sm:text-[30px] text-[25px] text-[#38879d]'>Become discoverable for all of our roles</p>
 
