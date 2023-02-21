870735ab5b5594a0615f5a51bafcaa0df58198fc
hisham <hishammpsn@gmail.com>
2023-02-21 19:50:24 +0530

    added form data and active navBar

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,56 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Form() {
+  const initialState = { email: "", firstName: "", lastName: "", telephone: "", cv: "" };
+  const [data, setData] = useState(initialState)
+
+const handleSubmit = e =>{
+  e.preventDefault();
+  console.log(data)
+  alert("Thank You ")
+}
   return (
     <div className='mx-3 md:mx-28'>
       <div className='text-primary mt-10'>
         <p className='font-sans text-[25px] md:text-[30px] font-bold '>Personal Details</p>
         <p className='text-[15px] md:text-[20px]'>Pleade complete the below form and attach cv.</p>
-        <form class="w-full  mt-10">
-          <div class="w-full md:w-[33%]  mb-6 md:mb-0">
-            <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+        <form className="w-full  mt-10"  onSubmit={handleSubmit}>
+          <div className="w-full md:w-[33%]  mb-6 md:mb-0">
+            <label className="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
               Email Address
             </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded-2xl py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="email" />
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded-2xl py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="email"
+              onChange={(e) => {
+                setData({ ...data, email: e.target.value })
+              }} />
           </div>
-          <div class="flex flex-wrap ">
-            <div class="w-full md:w-[33%]  mb-6 md:mb-0 ">
-              <label class="block tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+          <div className="flex flex-wrap ">
+            <div className="w-full md:w-[33%]  mb-6 md:mb-0 ">
+              <label className="block tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                 First Name
               </label>
-              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded-2xl py-3 px-4  leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" />
+              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded-2xl py-3 px-4  leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text"
+                onChange={(e) => {
+                  setData({ ...data, firstName: e.target.value })
+                }
+                }
+              />
             </div>
-            <div class="w-full md:w-[33%] mb-6 md:mb-0 md:pl-3">
-              <label class="block tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+            <div className="w-full md:w-[33%] mb-6 md:mb-0 md:pl-3">
+              <label className="block tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                 Last Name
               </label>
-              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded-2xl py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" />
-            </div>            <div class="w-full md:w-[33%] md:pl-3 mb-6 md:mb-0">
-              <label class="block tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded-2xl py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text"
+                onChange={(e) => {
+                  setData({ ...data, lastName: e.target.value })
+                }}
+              />
+            </div>
+            <div className="w-full md:w-[33%] md:pl-3 mb-6 md:mb-0">
+              <label className="block tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                 Telephone
               </label>
-              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded-2xl py-3 px-4  leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="number" />
-              <p class="text-red-500 text-xs italic">Please include your country area code.</p>
+              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded-2xl py-3 px-4  leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="number"
+                onChange={(e) => {
+                  setData({ ...data, telephone: e.target.value })
+                }
+                }
+              />
+              <p className="text-red-500 text-xs italic">Please include your country area code.</p>
             </div>
           </div>
-          <div class="w-full md:w-[33%]  mb-6 md:mb-0">
-            <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+          <div className="w-full md:w-[33%]  mb-6 md:mb-0">
+            <label className="block  tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
               Upload Cv
             </label>
-            <div class="flex  items-center justify-center ">
-              <label class="w-full flex flex-row justify-between items-center px-4 py-1 text-primary rounded-2xl bg-[#bfbfbf] shadow-lg tracking-wide uppercase border border-primary cursor-pointer hover:text-white">
-                <span class=" text-base leading-normal capitalize">Upload Cv</span>
-                <svg class="w-[32px] h-[32px]" xmlns="http://www.w3.org/2000/svg" width="37.5" height="37.491" viewBox="595 1430 37.5 37.491"><path d="m607.581 1440.697 4.294-4.313v17.982a1.875 1.875 0 0 0 3.75 0v-17.982l4.294 4.313a1.875 1.875 0 0 0 2.662 0 1.875 1.875 0 0 0 0-2.663l-7.5-7.5a1.875 1.875 0 0 0-.619-.393 1.875 1.875 0 0 0-1.425 0c-.23.089-.44.223-.618.393l-7.5 7.5a1.883 1.883 0 0 0 2.662 2.663Zm23.044 8.044c-1.036 0-1.875.84-1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-26.25a1.875 1.875 0 0 1-1.875-1.875v-11.25a1.875 1.875 0 1 0-3.75 0v11.25a5.625 5.625 0 0 0 5.625 5.625h26.25a5.625 5.625 0 0 0 5.625-5.625v-11.25c0-1.036-.84-1.875-1.875-1.875Z" fill-rule="evenodd" data-name="upload" /></svg>
-                <input type='file' class="hidden" />
+            <div className="flex  items-center justify-center ">
+              <label className="w-full flex flex-row justify-between items-center px-4 py-1 text-primary rounded-2xl bg-[#bfbfbf] shadow-lg tracking-wide uppercase border border-primary cursor-pointer hover:text-white">
+                <span className=" text-base leading-normal capitalize">Upload Cv</span>
+                <svg className="w-[32px] h-[32px]" xmlns="http://www.w3.org/2000/svg" width="37.5" height="37.491" viewBox="595 1430 37.5 37.491"><path d="m607.581 1440.697 4.294-4.313v17.982a1.875 1.875 0 0 0 3.75 0v-17.982l4.294 4.313a1.875 1.875 0 0 0 2.662 0 1.875 1.875 0 0 0 0-2.663l-7.5-7.5a1.875 1.875 0 0 0-.619-.393 1.875 1.875 0 0 0-1.425 0c-.23.089-.44.223-.618.393l-7.5 7.5a1.883 1.883 0 0 0 2.662 2.663Zm23.044 8.044c-1.036 0-1.875.84-1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-26.25a1.875 1.875 0 0 1-1.875-1.875v-11.25a1.875 1.875 0 1 0-3.75 0v11.25a5.625 5.625 0 0 0 5.625 5.625h26.25a5.625 5.625 0 0 0 5.625-5.625v-11.25c0-1.036-.84-1.875-1.875-1.875Z" fill-ule="evenodd" data-name="upload" /></svg>
+                <input type='file' className="hidden"
+                  onChange={(e) => {
+                    setData({ ...data, cv: e.target.files[0] })
+                  }}
+                />
               </label>
             </div>
-            <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">Select resume file (TXT, PDF or Word DOC)</p>
+            <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">Select resume file (TXT, PDF or Word DOC)</p>
           </div>
-        </form>
-      </div>
-      <button type='submit' className="bg-blue hover:bg-blue text-white font-bold mt-10 py-2 px-7 rounded-[12px]">
+          <button type='submit' className="bg-blue hover:bg-blue text-white font-bold mt-10 py-2 px-7 rounded-[12px]">
         Submit
       </button>
+        </form>
+      </div>
+      
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assests/logo.png'
 import SideNav from './SideNav'
 
 function Header({ SetsideNav, Navdata }) {
+    const [activeTab, setActiveTab] = useState('About Us')
     return (
         <div className=''>
-            <div class="relative">
-                <div class="mx-auto max-w-7xl px-6">
-                    <div class="flex items-center justify-between  py-6 md:justify-start md:space-x-10">
-                        <div class="flex justify-start lg:w-0 lg:flex-1">
+            <div className="relative">
+                <div className="mx-auto max-w-7xl px-6">
+                    <div className="flex items-center justify-between  py-6 md:justify-start md:space-x-10">
+                        <div className="flex justify-start lg:w-0 lg:flex-1">
                             <a href="#">
-                                <img class="h-8 w-auto sm:h-10 " src={logo} alt="" />
+                                <img className="h-8 w-auto sm:h-10 " src={logo} alt="" />
                             </a>
                         </div>
-                        <div class="-my-2 -mr-2 md:hidden">
+                        <div className="-my-2 -mr-2 md:hidden">
                             <button type="button"
                                 onClick={() => SetsideNav((prev) => !prev)}
-                                class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
-                                <span class="sr-only">Open menu</span>
-                                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
+                                <span className="sr-only">Open menu</span>
+                                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                                 </svg>
                             </button>
                         </div>
-                        <nav class="hidden space-x-10 md:flex">
+                        <nav className="hidden space-x-10 md:flex">
                             {
-                                Navdata.map((item) => (
-                                    <a href={item.url} class=" hover:text-gray-500 text-white text-[25px] hover:opacity-25">{item.item}</a>
+                                Navdata.map((item, i) => (
+                                    <a key={i} 
+                                    href={item.url} 
+                                    className={`hover:text-gray-500 text-white text-[25px] hover:opacity-25 ${activeTab===item.item && "font-semibold"}`}
+                                    onClick={()=> setActiveTab(item.item)}
+                                    >{item.item}</a>
                                 ))
                             }
 
                         </nav>
-                        <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-                            <a href="#" class="whitespace-nowrap font-medium text-[25px]  text-white hover:opacity-25">Login</a>
-                            <a href="#" class="ml-8 whitespace-nowrap text-[25px]  font-medium text-white hover:opacity-25">Sign Up</a>
+                        <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+                            <a href="#" className="whitespace-nowrap  text-[25px]  text-white hover:opacity-25">Login</a>
+                            <a href="#" className="ml-8 whitespace-nowrap text-[25px]  text-white hover:opacity-25">Sign Up</a>
 
                         </div>
                     </div>
